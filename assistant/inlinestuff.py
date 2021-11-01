a4036cac0160ef61b4a44ad9c36f40e82fd611dd
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-01 15:26:32 +0530

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -21,14 +21,14 @@ from . import *
 
 SUP_BUTTONS = [
     [
-        Button.url("• Repo •", url="https://github.com/TeamUltroid/Ultroid"),
-        Button.url("• Support •", url="t.me/UltroidSupport"),
+        Button.url("• Channel •", url="https://t.me/gd_film"),
+        Button.url("• Support •", url="t.me/KicchaRequest"),
     ],
 ]
 
-ofox = "https://telegra.ph/file/231f0049fcd722824f13b.jpg"
-gugirl = "https://telegra.ph/file/0df54ae4541abca96aa11.jpg"
-ultpic = "https://telegra.ph/file/4136aa1650bc9d4109cc5.jpg"
+CME Movies = "https://telegra.ph/file/ff9119f33fe25210a347a.jpg"
+Sachin S = "https://telegra.ph/file/fbf7d4ca07cbde2c6611c.jpg"
+Sachin ultpic = "https://telegra.ph/file/4315144f3d7c66432101d.jpg"
 
 api1 = base64.b64decode("QUl6YVN5QXlEQnNZM1dSdEI1WVBDNmFCX3c4SkF5NlpkWE5jNkZV").decode(
     "ascii"
