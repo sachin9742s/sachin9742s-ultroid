24fbc39174bd2b12ae06e471ac5f045bd39ced4f
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-01 15:04:12 +0530

    Update _help.py

--- assistant/manager/_help.py
+++ assistant/manager/_help.py
@@ -14,7 +14,7 @@ START = """
 ✘  /help : Get This Message.
 ✘  /repo : Get Bot's Repo..
 
-🧑‍💻 Join **@TheUltroid**
+🧑‍💻 Join My Boss Movie Request Group **@KicchaRequest**
 """
 
 ADMINTOOLS = """✘ **AdminTools** ✘
